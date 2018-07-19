05afdb35287f934f119d031e9df6b0be24a0af6d
Ankit Kumar <ankitkumar@CapitalMac.local>
2018-07-19 10:42:53 +0800

    Initial commit

---
+++ app/src/main/java/com/cogitator/fingerprintauthentication/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.cogitator.fingerprintauthentication
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar on 19/07/2018
+ */
+class MainActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/cogitator/fingerprintauthentication/SettingsActivity.kt
@@ -0,0 +1,5 @@
+package com.cogitator.fingerprintauthentication
+
+/**
+ * @author Ankit Kumar on 19/07/2018
+ */
