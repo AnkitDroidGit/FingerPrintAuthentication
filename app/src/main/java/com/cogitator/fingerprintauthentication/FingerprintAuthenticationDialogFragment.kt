d3ba5061590dc4dcb75dbb762a0ad51f14d6fbd6
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2018-07-19 12:28:04 +0800

    Added FingerprintAuthenticationDialogFragment

--- app/src/main/java/com/cogitator/fingerprintauthentication/FingerprintAuthenticationDialogFragment.kt
+++ app/src/main/java/com/cogitator/fingerprintauthentication/FingerprintAuthenticationDialogFragment.kt
@@ -1,7 +1,36 @@
 package com.cogitator.fingerprintauthentication
 
+import android.content.SharedPreferences
+import android.hardware.fingerprint.FingerprintManager
+import android.support.v4.app.DialogFragment
+import android.view.inputmethod.InputMethodManager
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.hardware.fingerprint.FingerprintManager
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+
 /**
  * @author Ankit Kumar on 19/07/2018
  */
-class FingerprintAuthenticationDialogFragment {
+class FingerprintAuthenticationDialogFragment :DialogFragment(), TextView.OnEditorActionListener,
+        FingerprintUiHelper.Callback{
+    private lateinit var callback: Callback
+    private lateinit var cryptoObject: FingerprintManager.CryptoObject
+    private lateinit var fingerprintUiHelper: FingerprintUiHelper
+    private lateinit var inputMethodManager: InputMethodManager
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private var stage = Stage.FINGERPRINT
 }
--- app/src/main/java/com/cogitator/fingerprintauthentication/SettingsActivity.kt
+++ app/src/main/java/com/cogitator/fingerprintauthentication/SettingsActivity.kt
@@ -1,5 +1,8 @@
 package com.cogitator.fingerprintauthentication
 
+import android.support.v7.app.AppCompatActivity
+
 /**
  * @author Ankit Kumar on 19/07/2018
  */
+class SettingsActivity : AppCompatActivity() {}
